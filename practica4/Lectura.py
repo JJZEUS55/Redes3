af4f24768c5fa1a0965e925f62e5af438c97f95e
JJZEUS55 <geoge55@hotmail.com>
2018-04-03 06:31:06 -0700

    Usuario conectados
    avance

--- practica4/Lectura.py
+++ practica4/Lectura.py
@@ -1,6 +1,7 @@
 import time
 from tkinter import *
 from tkinter import ttk
+import os
 
 root = Tk()
 topFrame = Frame(root)
@@ -48,23 +49,35 @@ dias["values"] = ["Mon",
                   "Sat",
                   "Sun"]
 
+def limpia():
+    os.system("cls" if os.name=="nt" else "clear")
+
 
 def busquedanombre():
+    limpia()
     openFile = open("vsftpd1.log", "r")
+    contador = 0
     nombre = usuarios.get()
+    numLineas = sum(1 for line in open("vsftpd1.log"))
     while 1:
         where = openFile.tell()
         line = openFile.readline()
         if not line:
             time.sleep(1)
-            openFile.seek(where)
+            if numLineas == contador:
+                break;
         else:
             if nombre in line:
+                contador += 1
                 print(line)
+            else:
+                contador +=1
 
 
 def busquedahora():
     openFile = open("vsftpd1.log", "r")
+    contador = 0
+    numLineas = sum(1 for line in open("vsftpd1.log"))
     hora = horaTxt.get()
     minutos = minTxt.get()
     horaCompleta = hora + ":" + minutos
@@ -73,30 +86,40 @@ def busquedahora():
         line = openFile.readline()
         if not line:
             time.sleep(1)
-            openFile.seek(where)
+            if numLineas == contador:
+                break;
         else:
             if horaCompleta in line:
+                contador += 1
                 print(line)
+            else:
+                contador += 1
 
 
 def busquedadia():
     openFile = open("vsftpd1.log", "r")
-    hora = horaTxt.get()
-    minutos = minTxt.get()
-    horaCompleta = hora + ":" + minutos
+    contador = 0
+    numLineas = sum(1 for line in open("vsftpd1.log"))
+    dia = dias.get()
     while 1:
         where = openFile.tell()
         line = openFile.readline()
         if not line:
             time.sleep(1)
-            openFile.seek(where)
+            if numLineas == contador:
+                break;
         else:
-            if horaCompleta in line:
+            if dia in line:
+                contador += 1
                 print(line)
+            else:
+                contador += 1
 
 
 def usuariosconectados():
     openFile = open("vsftpd1.log", "r")
+    contador = 0
+    numLineas = sum(1 for line in open("vsftpd1.log"))
     valores = usuarios["values"]
     print("Usuarios Conectados")
     while 1:
@@ -104,10 +127,14 @@ def usuariosconectados():
         line = openFile.readline()
         if not line:
             time.sleep(1)
-            openFile.seek(where)
+            if numLineas == contador:
+                break;
         else:
-            if valores[1] and "terminated" in line:
+            if "jj1" in line and "terminated" in line:
+                contador += 1
                 print(line)
+            else:
+                contador += 1
 
 
 btnBuscar = Button(topFrame, text="Buscar", command=usuariosconectados)
